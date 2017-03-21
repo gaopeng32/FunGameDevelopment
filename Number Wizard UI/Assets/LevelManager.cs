7d29ce86517f5ef003f6baf41a037b27a8f9535a
Peng Gao <gaopeng32@gmail.com>
2017-03-21 19:47:55 -0400

    Number Wizard UI game

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadLevel(string name) {
+		Debug.Log ("Level load requested for " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void quitRequest(string name) {
+		Debug.Log ("I want to quit!");
+		Application.Quit();
+	}
+}
---
+++ Number Wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAllowed; 
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		startGame ();
+	}
+
+	void startGame() {
+		max = 1000;
+		min = 1;
+		maxGuessesAllowed = 10;
+
+		nextGuess ();
+	}
+
+
+	public void guessHigher() {
+		min = guess + 1;
+		nextGuess();
+	}
+
+	public void guessLower() {
+		max = guess - 1;
+		nextGuess();
+	}
+
+	
+	void nextGuess() {
+//		guess = (max + min) / 2;
+		guess = Random.Range (min, max + 1);
+		text.text = guess.ToString();
+
+		maxGuessesAllowed--;
+		if (maxGuessesAllowed <= 0) {
+			// User win
+			Application.LoadLevel("Win");
+		}
+	}
+}
